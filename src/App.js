d80977b84b62c960c3e82def6cf2b98d932f666a
harshbix <kidabixson@gmail.com>
2023-08-14 14:59:21 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DictionaryApp from './dictionaryApp';
+
+function App() {
+  return (
+    <div className="App">
+      <DictionaryApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/dictionaryApp.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const DictionaryApp = () => {
+  const [word, setWord] = useState("");
+  const [meaning, setMeaning] = useState("");
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+      );
+
+      const firstMeaning =
+        response.data[0]?.meanings[0]?.definitions[0]?.definition ||
+        "Meaning not found";
+      setMeaning(firstMeaning);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setMeaning("Error fetching meaning");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Dictionary App</h1>
+      <input
+        type="text"
+        placeholder="Enter a word"
+        value={word}
+        onChange={(e) => setWord(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+      <div>
+        <h2>Meaning:</h2>
+        <p>{meaning}</p>
+      </div>
+    </div>
+  );
+};
+
+export default DictionaryApp;

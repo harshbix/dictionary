9db56d84bdc8663221e32c316452c00073aebc3e
harshbix <kidabixson@gmail.com>
2023-08-17 20:28:34 +0300

    added a search space and customized it

--- src/componets/dictionaryApp.jsx
+++ src/componets/dictionaryApp.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 
 function MyNav() {
   return (
@@ -8,51 +8,35 @@ function MyNav() {
         <div className="container">
           <a className="navbar-brand">Navbar</a>
           <form className="d-flex" role="dark-mode-toggle">
-          <select className="form-select form-select-sm border-0">
-            <option defaultValue>Serif</option>
-            <option value="1">Sans-Serif</option>
-          </select>
+            <select className="form-select form-select-sm border-0">
+              <option defaultValue>Serif</option>
+              <option value="1">Sans-Serif</option>
+            </select>
             <div className="form-check form-switch">
               <input
                 className="form-check-input mx-3"
                 type="checkbox"
                 id="flexSwitchCheckDefault"
-              /> 
+              />
             </div>
-            <label className="form-check-label mx-2 col">
-                🌙
-              </label>
+            <label className="form-check-label mx-2 col">🌙</label>
           </form>
         </div>
       </nav>
+      <div className="search mx-2">
+        <span className="fa fa-search"></span>
+        <input placeholder="Search word" />
+      </div>
     </div>
   );
 }
 
 const DictionaryApp = () => {
-  const [word, setWord] = useState("");
-  const [meaning, setMeaning] = useState("");
-
-  const handleSearch = async () => {
-    try {
-      const response = await axios.get(
-        `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
-      );
-
-      const firstMeaning =
-        response.data[0]?.meanings[0]?.definitions[0]?.definition ||
-        "Meaning not found";
-      setMeaning(firstMeaning);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      setMeaning("Error fetching meaning");
-    }
-  };
-
   return (
-    <div>
+    <>
       <MyNav />
-    </div>
+      <searchComponent />
+    </>
   );
 };
 
